004cc174fc3d32b7be36151c50687fc812ce848b
Artur <artur.h.0427@gmail.com>
2022-08-18 00:00:05 +0200

    Function added to calculate all inner values

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,8 @@
 /*- Imports -*/
 use std::{
     fmt,
-    iter::{
-        Iterator,
-        IntoIterator
-    }
 };
-use rand::{ Rng, thread_rng, distributions::uniform::SampleRange };
+use rand::{ Rng, thread_rng };
 
 /*- Constants -*/
 const ACTIVATION_FNS:&'static [(&'static str, fn(f32) -> f32)] = &[
@@ -24,7 +20,7 @@ const ACTIVATION_FNS:&'static [(&'static str, fn(f32) -> f32)] = &[
 ];
 
 /*- Structs, enums & unions -*/
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct NeuralNetwork {
     input: Vec<Neuron>,
     hidden:Vec<Vec<Neuron>>,
@@ -65,10 +61,11 @@ fn main() -> () {
 
     /*- Print the layers -*/
     println!("{network:#?}");
-    println!("{:#?}", sum_layer(&network, 3, 0));
-    println!("{:#?}", network);
+    println!(
+        "{:#?}",
+        calculate_all_inners(&network)
+    );
 }
-
 
 /*- Functions -*/
 fn get_layer<'lf>(network:&'lf NeuralNetwork, index:usize) -> Vec<Neuron> {
@@ -102,27 +99,62 @@ fn random_weights(len:usize) -> Vec<f32> {
         vec
     }
 }
-fn sum_layer(network:&NeuralNetwork, layer_index:usize, sum_for:usize) -> f32 {
-    /*- If user wants to sum input -*/
-    if layer_index == 0 {
-        let mut sum:f32 = 0.0f32;
-        for a in network.input.to_vec() { sum += a.inner; };
-        sum
-    }else {
-        let mut sum:f32 = 0.0f32;
 
-        /*- Get the layer -*/
-        let layer = network.hidden.get(layer_index - 1);
-        println!("layer {layer:?}");
-        match layer {
-            Some(e) => {
-                for a in e.iter() { sum += a.inner; };
-                sum
-            },
-            None => 0.0f32
-        }
+fn calculate_inner(network:&NeuralNetwork, layer_index:usize, neuron_index:usize) -> f32 {
+    /*- Get the layers -*/
+    let curr_layer = get_layer(network, layer_index);
+    let prev_layer = get_layer(network, layer_index - 1);
+
+    /*- Get the current neuron -*/
+    let neuron = &match curr_layer.get(neuron_index) {
+        Some(e) => e,
+        None => panic!("AWBDOIAWBDOIAWOIBDOAIWD")
+    };
+    
+    /*- Sum of all weights, biases and neuron inners -*/
+    let mut sum = neuron.inner;
+
+    /*- Loop through every neuron in the previous layer -*/
+    for previous_neuron in prev_layer {
+        /*- < previous_neuron.weights[neuron_index] > will grab the corresponding weight to this neuron -*/
+        sum += previous_neuron.inner * previous_neuron.weights[neuron_index];
     }
+
+    sum
 }
+fn calculate_all_inners(mut network:&NeuralNetwork) -> NeuralNetwork {
+    /*- Get the amount of layers in the network -*/
+    let network_layer_len:usize = network.hidden.len() + 1 /*- Input -*/ + 1 /*- Output -*/;
+    let mut all_layers:Vec<Vec<Neuron>> = Vec::with_capacity(network_layer_len);
+    let mut network = network.clone();
+
+    /*- Iterate over all the layers, skip input
+        because their inner should already be set -*/
+    for layer_index in 1..network_layer_len {
+        println!("CALCULATING: {layer_index}");
+        /*- Get the layer -*/
+        let layer = get_layer(&network, layer_index);
+        println!("{layer:?}");
+        let mut layer_mut = layer.clone();
+
+        /*- Iterate over all neurons -*/
+        for (neuron_index, neuron) in layer.iter().enumerate() {
+            layer_mut[neuron_index].inner = calculate_inner(&network, layer_index, neuron_index);
+        };
+
+        /*- Set the all neutrons' inners in the new layer -*/
+        if layer_index == network_layer_len - 1 {
+            network.output = layer_mut.clone();
+        }else {
+            network.hidden[layer_index - 1] = layer_mut.clone();
+        }
+    };
+
+    /*- Return -*/
+    network
+}
+
+
 
 /*- Implementations -*/
 impl NeuronDefaultTraits for Neuron {
